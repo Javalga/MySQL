b228e1518b5d821299f595759b2273065dcfc8de
Javier Alonso <jalonsogal@gmail.com>
2022-11-17 00:22:32 +0100

    a

--- day1/reto2/index.js
+++ day1/reto2/index.js
@@ -16,8 +16,10 @@ connection.connect(err => {
   }
 })
 
-let sql = ""
-
+let sql1 = "UPDATE * FROM marks SET mark = 0"
+let sql2 = "SELECT first_name, last_name FROM students;"
+let sql3 = "SELECT * from codenotch2.teachers"
+let sql4 = "DELETE FROM marks WHERE date < 2012-11-16"
 // connection.query(sql, (err, result) => {
 //   if (err) {
 //     console.log(err);
